2a4b7e8abcaaa18326bb5179f9f247b43a0460aa
me89 <michael_ger@gmx.net>
2016-12-19 12:01:10 +0100

    Models + Skins etc

---
+++ Assets/Skripts/groundScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundScript : MonoBehaviour {
+
+	public float scaleFactor = 5.0f;
+	Material mat;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTextureScale = new Vector2 (transform.localScale.x / scaleFactor , transform.localScale.z / scaleFactor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
