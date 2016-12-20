7ee815885e30ffce3db0957b5b24a5928bfcb8e0
kreusch <Timo@dhcp-10-180-67-158.dynamic.eduroam.mwn.de>
2016-12-20 11:55:49 +0100

    Added Skripts
    Alle Skripte vom alten TowerTK branch

---
+++ Assets/Skripts/Ghost.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghost : Enemy {
+
+	int startingHealth = 10;
+	int ghostSpeed = 7;
+	int ghostDamage = 3;
+	int ghostReward = 1;
+
+	void Start () {
+		currentHealth = startingHealth; /*Health setzen*/
+		speed = ghostSpeed;
+		attackDamage = ghostDamage;
+		reward = ghostReward;
+
+		target = GameObject.Find ("Path").transform.GetChild (1);
+		agent = GetComponent<NavMeshAgent> ();
+		agent.speed = speed;
+		gateHealth = target.GetComponent <GateHealth>(); /*Zugriff aufs Script Gate Health*/
+	}
+}
