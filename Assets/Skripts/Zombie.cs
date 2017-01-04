d13def559308b789cafe8cc1692e63d3aac93784
me89 <michael_ger@gmx.net>
2017-01-04 11:30:32 +0100

    animate

--- Assets/Skripts/Zombie.cs
+++ Assets/Skripts/Zombie.cs
@@ -7,18 +7,31 @@ public class Zombie : Enemy {
 	int zombieSpeed = 1;
 	int zombieDamage = 1;
 	int zombieValue = 1;
+	bool spawning = true;
+	bool isAlive  = true;
+
 
 	void Start () {
+		
 		currentHealth = startingHealth; /*Health setzen*/
 		speed = zombieSpeed;
 		attackDamage = zombieDamage;
 		enemyValue = zombieValue;
-
 		target = GameObject.Find ("Path").transform.GetChild (1);
 		agent = GetComponent<NavMeshAgent> ();
 		agent.speed = speed;
 		gateHealth = target.GetComponent <GateHealth>(); /*Zugriff aufs Script Gate Health*/
+		spawning = false;
 	}
+	void Update(){
+		
+	}
+
+	public void kill(){
+		isAlive = false;
+	}
+
+
 
 
 }
